0cd6182520cc046a84136b537894f929ace79c78
carlosalegriam <calegriam@protonmail.com>
2023-06-30 21:31:33 -0400

    Base de Datos definida

--- todolist/settings.py
+++ todolist/settings.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'calegriam_todolist',
+        'USER': 'calegriam_admin',
+        'PASSWORD': 'M@1234567-8',
+        'HOST': 'postgresql-calegriam.alwaysdata.net',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
